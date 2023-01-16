327f50edcdeccf7faef90c9cc6c3de451ff12b03
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2023-01-16 10:13:35 -0600

    Add missing error message on JS.

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ const submitButton = document.querySelector(".submit");
 submitButton.addEventListener("click", () => {
     let email = emailInput.value;
     let validMailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (email.match(validMailFormat)) {
+
+    if (email == "") { 
+        errorText.classList.remove("invisible"); 
+        emailInput.classList.add("error");
+        errorText.textContent = "Whoops! It looks like you forgot to add your email."
+    } else if (email.match(validMailFormat)) {
         errorText.classList.add("invisible"); 
         emailInput.classList.remove("error");
     } else {
         errorText.classList.remove("invisible"); 
         emailInput.classList.add("error");
+        errorText.textContent = "Please provide a valid email address."
     }
 });
